bf592b21a904380098f34bf3f7a72405695a4717
David Neuhaus <davidneuhaus@web.de>
2023-03-06 21:47:01 +0100

    change metronome icon

--- web/src/components/Practice.tsx
+++ web/src/components/Practice.tsx
@@ -17,6 +17,34 @@ import styles from "./Practice.module.css";
 
 type Props = {};
 
+function MetronomeIcon() {
+  return (
+    <svg
+      fill="#000000"
+      height="14px"
+      width="14px"
+      version="1.1"
+      id="Capa_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 213.605 213.605"
+      xmlSpace="preserve"
+    >
+      <g>
+        <path
+          d="M200.203,161.656L143.86,4.962C142.79,1.985,139.966,0,136.803,0h-60c-3.164,0-5.987,1.985-7.058,4.962L13.402,161.656
+		c-0.292,0.814-0.442,1.672-0.442,2.538v41.912c0,4.142,3.358,7.5,7.5,7.5h172.686c4.142,0,7.5-3.358,7.5-7.5v-41.912
+		C200.646,163.329,200.496,162.47,200.203,161.656z M82.076,15h49.453l50.949,141.694h-70.676v-4.861h7.5c2.761,0,5-2.239,5-5
+		s-2.239-5-5-5h-7.5v-7.36h7.5c2.761,0,5-2.239,5-5s-2.239-5-5-5h-7.5v-7.36h7.5c2.761,0,5-2.239,5-5s-2.239-5-5-5h-7.5v-7.361h7.5
+		c2.761,0,5-2.239,5-5s-2.239-5-5-5h-7.5V47.333c0-2.761-2.239-5-5-5s-5,2.239-5,5v42.418h-7.5c-2.761,0-5,2.239-5,5s2.239,5,5,5
+		h7.5v7.361h-7.5c-2.761,0-5,2.239-5,5s2.239,5,5,5h7.5v7.36h-7.5c-2.761,0-5,2.239-5,5s2.239,5,5,5h7.5v7.36h-7.5
+		c-2.761,0-5,2.239-5,5s2.239,5,5,5h7.5v4.861H31.127L82.076,15z M27.96,198.605v-26.912h157.686v26.912H27.96z"
+        />
+      </g>
+    </svg>
+  );
+}
+
 function Practice(props: Props) {
   const { dispatch } = useContext(AppStateContext);
   const session = usePracticeSession();
@@ -95,12 +123,14 @@ function Practice(props: Props) {
         {currentlyPracticing()}
         <div className={styles.activeTools}></div>
         <div className={styles.toolsBox}>
+          {/* TODO think: Tuner needed? */}
           <button className={styles.tool}>
             <FontAwesomeIcon icon={solid("music")} />{" "}
             {/* TODO better icon for pedal note */}
           </button>
           <button className={styles.tool}>
-            <FontAwesomeIcon icon={solid("radio")} /> {/* TODO tuner icon */}
+            <MetronomeIcon />
+            {/* TODO better icon for metronome */}
           </button>
           <button className={styles.tool}>
             <FontAwesomeIcon icon={solid("bell")} />
